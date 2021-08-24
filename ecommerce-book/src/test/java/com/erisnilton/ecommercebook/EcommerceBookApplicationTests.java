0d7632afef454a0373f9793f4e5d5ca8b7f79023
Erisnilton Freitas <erisniltongf@gmail.com>
2021-08-24 19:16:25 -0300

    Inicial Project ecommerce-book for to learn Spring Boot

---
+++ ecommerce-book/src/test/java/com/erisnilton/ecommercebook/EcommerceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erisnilton.ecommercebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
