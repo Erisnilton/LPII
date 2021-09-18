8273ccf3da22ce8ca69ff4aaa131ac1cfab470c7
Erisnilton Freitas <erisniltongf@gmail.com>
2021-09-18 13:55:53 -0300

    update projet

--- e-commerce-book/src/main/java/br/edu/ifce/lp2/model/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifce.lp2.model.repository;
-
-import br.edu.ifce.lp2.model.entities.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository extends MongoRepository<Client, String> {
-
-    Client findByName(String name);
-}
--- e-commerce-book/src/main/java/br/edu/ifce/lp2/model/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifce.lp2.model.repository;
-
-import br.edu.ifce.lp2.model.entities.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
