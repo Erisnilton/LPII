6125a8641eaf0471271093f5dedc91d8ceb66a47
erisnilton <erisniltongf@gmail.com>
2021-12-04 16:38:03 -0300

    modify repository mongodb

--- barbearia/repository.mongodb/src/main/java/br/com/projeto/lp2/mongo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.projeto.lp2.mongo;
-
-import br.com.projeto.lp2.core.domain.User;
-import br.com.projeto.lp2.core.ports.driven.repository.UserRepositoryPort;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Long>, UserRepositoryPort {
-}
