1f7c32c91ffc5ad9095ca164ef3f06c33e5269a5
erisnilton <erisniltongf@gmail.com>
2021-12-07 09:48:41 -0300

    return response status code correctly

--- barbearia/api.rest/src/main/java/br/com/projeto/lp2/controller/UserController.java
+++ barbearia/api.rest/src/main/java/br/com/projeto/lp2/controller/UserController.java
@@ -4,10 +4,14 @@ import br.com.projeto.lp2.controller.request.UserRequest;
 import br.com.projeto.lp2.controller.response.UserResponse;
 import br.com.projeto.lp2.core.domain.User;
 import br.com.projeto.lp2.core.ports.driver.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
@@ -20,40 +24,45 @@ public record UserController(
 )
 {
     @PostMapping
-    public UserResponse post(@RequestBody UserRequest request) {
+    public ResponseEntity<UserResponse> post(@RequestBody UserRequest request, HttpServletResponse response) {
         var user = request.toUser();
-        User userSaved = createUserPort.apply(user);
-        return new UserResponse().fromUser(userSaved);
+        var userSaved = createUserPort.apply(user);
+        var userResponse = new UserResponse().fromUser(userSaved);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("{/id}").buildAndExpand(userSaved.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(userResponse);
     }
 
     @GetMapping("{id}")
-    public UserResponse getUserById(@PathVariable String id) {
+    public ResponseEntity<UserResponse> getUserById(@PathVariable String id) {
         User user = getUserByIdPort.apply(id);
-        return new UserResponse().fromUser(user);
+        return ResponseEntity.ok(new UserResponse().fromUser(user));
     }
 
     @GetMapping
-    public List<UserResponse> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         var users = getUsersPort.apply();
-        return users.stream().map(user -> new UserResponse().fromUser(user)).toList();
+        return ResponseEntity.ok(users.stream().map(user -> new UserResponse().fromUser(user)).toList());
     }
 
     @PutMapping("{id}")
-    public UserResponse updateUser(@PathVariable String id, @RequestBody UserRequest request) {
+    public ResponseEntity<UserResponse> updateUser(@PathVariable String id, @RequestBody UserRequest request) {
         if(request == null || id == null) {
             throw new IllegalArgumentException("Erro ao atualizar o usuario");
         }
         User user = request.toUser();
         User userUpdated = upadateUser.apply(id, user);
-        return new UserResponse().fromUser(userUpdated);
+        return ResponseEntity.ok(new UserResponse().fromUser(userUpdated));
     }
 
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable String id) {
         if(id == null) {
             throw new IllegalArgumentException("Usuario Inexistente");
         }
         deleteUserPort.apply(id);
     }
-
 }
