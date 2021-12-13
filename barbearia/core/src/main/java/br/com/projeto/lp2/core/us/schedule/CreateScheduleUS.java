6d07b8529acc88b3c4d9274252b02c530d399324
Erisnilton Freitas <erisniltongf@gmail.com>
2021-12-13 16:54:28 -0300

    create ScheduleRepository

--- barbearia/core/src/main/java/br/com/projeto/lp2/core/us/schedule/CreateScheduleUS.java
+++ barbearia/core/src/main/java/br/com/projeto/lp2/core/us/schedule/CreateScheduleUS.java
@@ -1,9 +1,10 @@
 package br.com.projeto.lp2.core.us.schedule;
 
 import br.com.projeto.lp2.core.domain.Schedule;
+import br.com.projeto.lp2.core.ports.driven.repository.schedule.ScheduleRepositoryPort;
 import br.com.projeto.lp2.core.ports.driver.schedule.CreateSchedulePort;
 
-public record CreateScheduleUS() implements CreateSchedulePort {
+public record CreateScheduleUS(ScheduleRepositoryPort repository) implements CreateSchedulePort {
     @Override
     public Schedule apply(Schedule schedule) {
         return null;
